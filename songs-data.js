4b8fcc684e8d4e0b8d6357e8d83267b15419bce0
catcrol78 <157789583+catcrol78@users.noreply.github.com>
2026-02-15 23:41:15 +0300

    Update songs-data.js
    версия

--- songs-data.js
+++ songs-data.js
@@ -7,29 +7,11 @@ const songsDataFromExternal = [
     },
     "artist": "Manu Chao",
     "youtubeId": "rs6Y4kZ8qtw",
-    "cover": "https://img.youtube.com/vi/rs6Y4kZ8qtw/mqdefault.jpg",
     "level": ["A1"],
-    "themes": ["naturaleza", "tiempo atmosférico"],
-    "grammar": ["gustar"],
-    "vocabulary": ["viajar", "soñar", "correr", "volver", "moto", "viento", "lluvia", "montaña", "noche", "cena", "vecino", "cocina", "guitarra", "canela", "fuego", "avión", "mañana", "¿qué voy a hacer?"],
-    "culture": {
-      "tags": ["holidays", "traditions", "geography", "cities", "food", "music"],
-      "items": ["La Habana", "Guatemala", "La Coruña", "Malasaña (район в Мадриде)"]
-    },
-    "restrictions": {
-      "age": "16+",
-      "containsOtherLanguages": true,
-      "profanity": "none",
-      "sensitiveTopics": [],
-      "note": "Есть части на французском языке"
-    },
     "lyrics": [
-      {"time": "", "text": "¿Qué horas son, mi corazón?"},
-      {"time": "", "text": "Te lo dije bien clarito"},
-      {"time": "", "text": "Permanece a la escucha"},
-      {"time": "", "text": "Permanece a la escucha"}
+      {"text": "Me gustas tú"},
+      {"text": "Me gustas tú, Manu Chao"}
     ],
-    "analysis": [],
     "tasks": []
   }
 ];
