c7bb80c0ed46527e96ee685d832c9583e4de584b
catcrol78 <157789583+catcrol78@users.noreply.github.com>
2026-02-17 22:20:42 +0300

    Update songs-data.js

--- songs-data.js
+++ songs-data.js
@@ -1,368 +1,30 @@
 // Автосгенерировано из admin.html
 const songsDataFromExternal = [
   {
-    "id": 1,
+    "id": 6,
     "title": {
-      "ru": "мне нравишься ты",
-      "es": "Me gustas tú"
+      "ru": "Песня1",
+      "es": "песня1 исп"
     },
-    "artist": "Manu Chao",
-    "youtubeId": "rs6Y4kZ8qtw",
-    "cover": "https://img.youtube.com/vi/rs6Y4kZ8qtw/mqdefault.jpg",
+    "artist": "исполнитель1",
+    "youtubeId": "G6OYDUomYwI",
+    "cover": "https://img.youtube.com/vi/G6OYDUomYwI/mqdefault.jpg",
     "level": [
       "A1"
     ],
     "themes": [
-      "naturaleza",
-      "tiempo atmosférico"
+      "тема1",
+      "тема11",
+      "тема111"
     ],
     "grammar": [
-      "gustar"
-    ],
-    "vocabulary": [
-      "viajar",
-      "soñar",
-      "correr",
-      "volver",
-      "moto",
-      "viento",
-      "lluvia",
-      "montaña",
-      "noche",
-      "cena",
-      "vecino",
-      "cocina",
-      "guitarra",
-      "canela",
-      "fuego",
-      "avión",
-      "mañana",
-      "¿qué voy a hacer?"
-    ],
-    "culture": {
-      "tags": [
-        "holidays",
-        "traditions",
-        "geography",
-        "cities",
-        "food",
-        "music"
-      ],
-      "items": [
-        "La Habana",
-        "Guatemala",
-        "La Coruña",
-        "Malasaña (район в Мадриде)"
-      ]
-    },
-    "restrictions": {
-      "age": "16+",
-      "containsOtherLanguages": true,
-      "profanity": "none",
-      "sensitiveTopics": [],
-      "note": "Есть части на французском языке"
-    },
-    "lyrics": [
-      {
-        "time": "0:00",
-        "text": "¿Qué horas son, mi corazón?"
-      },
-      {
-        "time": "0:10",
-        "text": "Te lo dije bien clarito"
-      },
-      {
-        "time": "",
-        "text": "Permanece a la escucha"
-      },
-      {
-        "time": "",
-        "text": "Permanece a la escucha"
-      },
-      {
-        "time": "",
-        "text": "Doce de la noche en La Habana, Cuba"
-      },
-      {
-        "time": "",
-        "text": "Once de la noche en San Salvador, El Salvador"
-      },
-      {
-        "time": "",
-        "text": "Once de la noche en Managua, Nicaragua"
-      },
-      {
-        "time": "",
-        "text": "Me gustan los aviones, me gustas tú"
-      },
-      {
-        "time": "",
-        "text": "Me gusta viajar, me gustas tú"
-      },
-      {
-        "time": "",
-        "text": "Me gusta la mañana, me gustas tú"
-      },
-      {
-        "time": "",
-        "text": "Me gusta el viento, me gustas tú"
-      },
-      {
-        "time": "",
-        "text": "Me gusta soñar, me gustas tú"
-      },
-      {
-        "time": "",
-        "text": "Me gusta la mar, me gustas tú"
-      },
-      {
-        "time": "",
-        "text": "¿Qué voy a hacer?, je ne sais pas"
-      },
-      {
-        "time": "",
-        "text": "¿Qué voy a hacer?, je ne sais plus"
-      },
-      {
-        "time": "",
-        "text": "¿Qué voy a hacer?, je suis perdu"
-      },
-      {
-        "time": "",
-        "text": "¿Qué horas son, mi corazón?"
-      },
-      {
-        "time": "",
-        "text": "Me gusta la moto, me gustas tú"
-      },
-      {
-        "time": "",
-        "text": "Me gusta correr, me gustas tú"
-      },
-      {
-        "time": "",
-        "text": "Me gusta la lluvia, me gustas tú"
-      },
-      {
-        "time": "",
-        "text": "Me gusta volver, me gustas tú"
-      },
-      {
-        "time": "",
-        "text": "Me gusta marihuana, me gustas tú"
-      },
-      {
-        "time": "",
-        "text": "Me gusta Colombiana, me gustas tú"
-      },
-      {
-        "time": "",
-        "text": "Me gusta la montaña, me gustas tú"
-      },
-      {
-        "time": "",
-        "text": "Me gusta la noche (me gustas tú)"
-      },
-      {
-        "time": "",
-        "text": "¿Qué voy a hacer?, je ne sais pas"
-      },
-      {
-        "time": "",
-        "text": "¿Qué voy a hacer?, je ne sais plus"
-      },
-      {
-        "time": "",
-        "text": "¿Qué voy a hacer?, je suis perdu"
-      },
-      {
-        "time": "",
-        "text": "¿Qué horas son, mi corazón?"
-      }
-    ],
-    "pdf": "",
-    "analysis": [],
-    "tasks": [
-      {
-        "title": {
-          "ru": "Аудирование",
-          "es": "audirovanie"
-        },
-        "instruction": {
-          "ru": "Прослушай песню и найди все формы глагола \"gustar\". Выпиши их.\n",
-          "es": " "
-        },
-        "type": "listening",
-        "content": "Прослушай песню и найди все формы глагола \"gustar\". Выпиши их.",
-        "wordBank": [
-          "gusta",
-          "gustan",
-          "gustas",
-          "gustamos"
-        ],
-        "answer": "gusta, gustan, gustas, gustamos"
-      },
-      {
-        "title": {
-          "ru": "",
-          "es": ""
-        },
-        "instruction": {
-          "ru": "скачажи что тебе нравится",
-          "es": ""
-        },
-        "type": "speaking",
-        "content": "",
-        "wordBank": [],
-        "answer": ""
-      },
-      {
-        "title": {
-          "ru": "читаем",
-          "es": ""
-        },
-        "instruction": {
-          "ru": "прочитать любую статью про густар",
-          "es": ""
-        },
-        "type": "reading",
-        "content": "",
-        "wordBank": [],
-        "answer": ""
-      },
-      {
-        "title": {
-          "ru": "письмо",
-          "es": ""
-        },
-        "instruction": {
-          "ru": "выпиши слово гутсра пять раз",
-          "es": ""
-        },
-        "type": "writing",
-        "content": "",
-        "wordBank": [],
-        "answer": ""
-      },
-      {
-        "title": {
-          "ru": "",
-          "es": ""
-        },
-        "instruction": {
-          "ru": "",
-          "es": ""
-        },
-        "type": "gapfill",
-        "text": "Yo ___ (ir) a la playa. Tú ___ (bailar) muy bien.",
-        "answers": [
-          "voy",
-          "bailas"
-        ],
-        "options": [
-          [
-            "voy",
-            "vas",
-            "va"
-          ],
-          [
-            "bailo",
-            "bailas",
-            "baila"
-          ]
-        ]
-      },
-      {
-        "title": {
-          "ru": "",
-          "es": ""
-        },
-        "instruction": {
-          "ru": "пройди викторину",
-          "es": ""
-        },
-        "type": "quiz",
-        "questions": [
-          {
-            "question": "Кто здесь?",
-            "options": [
-              "собака",
-              "кошка"
-            ],
-            "correct": 0
-          },
-          {
-            "question": "Сколько будет 2 + 2?",
-            "options": [
-              "3",
-              "4",
-              "5"
-            ],
-            "correct": 1
-          }
-        ]
-      },
-      {
-        "title": {
-          "ru": "",
-          "es": ""
-        },
-        "instruction": {
-          "ru": "найди пары",
-          "es": ""
-        },
-        "type": "match",
-        "pairs": [
-          {
-            "left": "casa",
-            "right": "дом"
-          },
-          {
-            "left": "perro",
-            "right": "собака"
-          },
-          {
-            "left": "gato",
-            "right": "кот"
-          },
-          {
-            "left": "sol",
-            "right": "солнце"
-          },
-          {
-            "left": "luna",
-            "right": "луна"
-          }
-        ]
-      }
-    ]
-  },
-  {
-    "id": 2,
-    "title": {
-      "ru": "танцуем",
-      "es": "Песня для проверки 1"
-    },
-    "artist": "Исполнитель 1",
-    "youtubeId": "yv-DcNTOMp4",
-    "cover": "https://img.youtube.com/vi/yv-DcNTOMp4/mqdefault.jpg",
-    "level": [
-      "A2"
-    ],
-    "themes": [
-      "amor",
-      "baile",
-      "cuerpo"
-    ],
-    "grammar": [
-      "грамматика 1",
-      "грамматика2",
-      "грамматика3"
+      "грамматика1",
+      "грамматика11"
     ],
     "vocabulary": [
       "лексика1",
-      "лексика2",
-      "лексика3"
+      "лексика11",
+      "лексика111"
     ],
     "culture": {
       "tags": [
@@ -375,38 +37,25 @@ const songsDataFromExternal = [
         "music"
       ],
       "items": [
-        "культура1",
-        "культура2",
-        "культура3"
+        "конкретика1",
+        "конкретика11"
       ]
     },
     "restrictions": {
-      "age": "12+",
-      "containsOtherLanguages": true,
+      "age": "16+",
+      "containsOtherLanguages": false,
       "profanity": "none",
       "sensitiveTopics": [],
-      "note": "Ограничение проверка"
+      "note": "есть огранчиения"
     },
     "lyrics": [
       {
-        "time": "0:00",
-        "text": "intro"
+        "time": "",
+        "text": "первая строчка"
       },
       {
-        "time": "0:10",
-        "text": "начало песни"
-      },
-      {
-        "time": "0:20",
-        "text": "первая часть песни"
-      },
-      {
-        "time": "0:30",
-        "text": "третья часть песни"
-      },
-      {
-        "time": "0:40",
-        "text": "конец песни"
+        "time": "",
+        "text": "вторая строчка"
       }
     ],
     "pdf": "",
@@ -421,23 +70,15 @@ const songsDataFromExternal = [
           "ru": "",
           "es": ""
         },
-        "type": "gapfill",
-        "text": "апоарп ___ ргорговыр___",
-        "answers": [
-          "voy",
-          "vas"
-        ],
-        "options": [
-          [
-            "voy",
-            "vas",
-            "va"
-          ],
-          [
-            "bailo",
-            "bailas",
-            "baila"
-          ]
+        "type": "flashcards",
+        "flashcards": [
+          {
+            "es": "слово",
+            "ru": "перевод",
+            "example": "пример",
+            "example_translation": "перевод примера",
+            "transcription": ""
+          }
         ]
       },
       {
@@ -452,91 +93,56 @@ const songsDataFromExternal = [
         "type": "match",
         "pairs": [
           {
-            "left": "casa",
-            "right": "дом"
+            "left": "слово1",
+            "right": "перевод1"
           },
           {
-            "left": "nada",
-            "right": "нчиего"
-          },
-          {
-            "left": "casa",
-            "right": "дом"
-          },
-          {
-            "left": "nada",
-            "right": "нчиего"
-          }
-        ]
-      },
-      {
-        "title": {
-          "ru": "",
-          "es": ""
-        },
-        "instruction": {
-          "ru": "",
-          "es": ""
-        },
-        "type": "listening",
-        "content": "",
-        "wordBank": [],
-        "answer": ""
-      },
-      {
-        "title": {
-          "ru": "",
-          "es": ""
-        },
-        "instruction": {
-          "ru": "",
-          "es": ""
-        },
-        "type": "flashcards",
-        "flashcards": [
-          {
-            "es": "слово1",
-            "ru": "перевод1",
-            "example": "пример1",
-            "example_translation": "перевод примера1",
-            "transcription": ""
+            "left": "слово2",
+            "right": "перевод2"
           }
         ]
       }
     ]
   },
   {
-    "id": 3,
+    "id": 7,
     "title": {
-      "ru": "Дикий ангел",
-      "es": "Cambio Dolor"
+      "ru": "песня2",
+      "es": "песня исп2"
     },
-    "artist": "Natalia Oreiro",
-    "youtubeId": "G6OYDUomYwI",
-    "cover": "https://img.youtube.com/vi/G6OYDUomYwI/mqdefault.jpg",
-    "level": [
-      "A1"
-    ],
+    "artist": "исполнитель2",
+    "youtubeId": "yv-DcNTOMp4",
+    "cover": "https://img.youtube.com/vi/yv-DcNTOMp4/mqdefault.jpg",
+    "level": [],
     "themes": [
-      "чучи1",
-      "чучи2",
-      "чучи3"
+      "тема2",
+      "тема22",
+      "тема222"
     ],
     "grammar": [
-      "конкретика1",
-      "конкретика2"
+      "грамматика2",
+      "грамматика22",
+      "грамматика222"
     ],
     "vocabulary": [
-      "лексика1",
-      "лексика2"
+      "лексика2",
+      "лексика22",
+      "лексика222"
     ],
     "culture": {
       "tags": [
+        "holidays",
+        "traditions",
+        "geography",
+        "cities",
+        "neighborhoods",
+        "food",
         "music"
       ],
       "items": [
-        "КОнкретика 2",
-        "конкретика3"
+        "культура2",
+        "культура22",
+        "культура222"
       ]
     },
     "restrictions": {
@@ -544,72 +150,20 @@ const songsDataFromExternal = [
       "containsOtherLanguages": false,
       "profanity": "none",
       "sensitiveTopics": [],
-      "note": ""
+      "note": "огарничение2"
     },
     "lyrics": [
       {
-        "time": "0:00",
-        "text": "Juraría que no sé bien lo que quiero"
-      },
-      {
-        "time": "0:02",
-        "text": "pero sé que moriría"
+        "time": "",
+        "text": "текст2"
       },
       {
         "time": "",
-        "text": "si me quedo en la mitad"
-      },
-      {
-        "time": "0:05",
-        "text": "por eso vuelo a otros senderos"
+        "text": "текст22"
       },
       {
         "time": "",
-        "text": "para conocer el mundo de verdad"
-      },
-      {
-        "time": "",
-        "text": "Aún no es tarde pero así me estoy sintiendo"
-      },
-      {
-        "time": "",
-        "text": "y aparecen tantos miedos"
-      },
-      {
-        "time": "",
-        "text": "que no me dejan pensar"
-      },
-      {
-        "time": "",
-        "text": "y tengo sueños de amores nuevos"
-      },
-      {
-        "time": "",
-        "text": "y me cuesta imaginar lo que vendrá."
-      },
-      {
-        "time": "",
-        "text": "Cambio dolor por libertad"
-      },
-      {
-        "time": "",
-        "text": "cambio heridas por un sueño"
-      },
-      {
-        "time": "",
-        "text": "que me ayude a continuar"
-      },
-      {
-        "time": "",
-        "text": "cambio dolor."
-      },
-      {
-        "time": "",
-        "text": "Felicidad que la suerte sea suerte"
-      },
-      {
-        "time": "",
-        "text": "y no algo que he de alcanzar…"
+        "text": "текст222"
       }
     ],
     "pdf": "",
@@ -617,117 +171,20 @@ const songsDataFromExternal = [
     "tasks": [
       {
         "title": {
-          "ru": "хаха",
-          "es": ""
+          "ru": "письмо",
+          "es": "письмо исп"
         },
         "instruction": {
-          "ru": "скажи куку",
-          "es": ""
+          "ru": "псиать",
+          "es": "писать исп"
         },
-        "type": "speaking",
-        "content": "",
-        "wordBank": [],
-        "answer": ""
-      }
-    ]
-  },
-  {
-    "id": 5,
-    "title": {
-      "ru": "Я прошу у Бога",
-      "es": "A Dios Le Pido"
-    },
-    "artist": "Juanes",
-    "youtubeId": "kMIaYXxLnUA",
-    "cover": "https://img.youtube.com/vi/kMIaYXxLnUA/mqdefault.jpg",
-    "level": [
-      "B1"
-    ],
-    "themes": [
-      "amor",
-      "deseos",
-      "familia"
-    ],
-    "grammar": [
-      "Presente de Subjuntivo"
-    ],
-    "vocabulary": [
-      "despertarse",
-      "la luz",
-      "la mirada",
-      "pedir",
-      "el alma",
-      "descansar",
-      "amar",
-      "quedarse",
-      "recordar",
-      "el pueblo",
-      "la gente"
-    ],
-    "culture": {
-      "tags": [
-        "traditions",
-        "religion"
-      ],
-      "items": [
-        "Colombia",
-        "Voseo (использование vos)"
-      ]
-    },
-    "restrictions": {
-      "age": "16+",
-      "note": "Используется форма 'vos' (sos, enamorás)"
-    },
-    "lyrics": [
-      {
-        "time": "0:18",
-        "text": "Que mis ojos se despierten con la luz de tu mirada"
-      },
-      {
-        "time": "0:23",
-        "text": "Yo a Dios le pido"
-      },
-      {
-        "time": "0:27",
-        "text": "Que mi madre no se muera y que mi padre me recuerde"
-      },
-      {
-        "time": "0:32",
-        "text": "A Dios le pido"
-      },
-      {
-        "time": "0:36",
-        "text": "Que te quedes a mi lado y que más nunca te me vayas mi vida"
-      },
-      {
-        "time": "0:41",
-        "text": "A Dios le pido"
-      },
-      {
-        "time": "0:45",
-        "text": "Que mi alma no descanse cuando de amarte se trate mi cielo"
-      },
-      {
-        "time": "0:50",
-        "text": "A Dios le pido"
-      }
-    ],
-    "tasks": [
-      {
-        "title": {
-          "ru": "Субхунтиво",
-          "es": "Subjuntivo"
-        },
-        "type": "gapfill",
-        "instruction": {
-          "ru": "Поставь глагол в Subjuntivo",
-          "es": ""
-        },
-        "text": "Que mis ojos se ___ (despertar). Que mi madre no se ___ (morir).",
-        "answers": [
-          "despierten",
-          "muera"
-        ]
+        "type": "writing",
+        "content": "написать письмо",
+        "wordBank": [
+          "банк слов1",
+          "банк слов2"
+        ],
+        "answer": "праивльный ответ "
       }
     ]
   }
